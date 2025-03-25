dcc422e6f3ff4f3db457d863382c4a6b682a9e58
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-03-25 19:26:07 +0000

    fav.js

---
+++ fav.js
@@ -0,0 +1,5 @@
+document.addEventListener ('DOMContentLoaded', function ()){
+    const favouritesContainer = document.querySelector(".my-fav")
+
+    let favouritesContainer = document.querySelector(".my-fav")
+}
