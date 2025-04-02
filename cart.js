b74ce4a7246b2aad227aefa33ec84aeb76778573
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-04-02 21:19:00 +0100

    cart update

--- cart.js
+++ cart.js
@@ -1,13 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const messageBox = document.querySelector(".Message");
-    const charCount = document.createElement("div");
-    messageBox.parentNode.appendChild(charCount);
 
-    const maxChars = 300;
-
-    messageBox.addEventListener("input", function() {
-        const remainingChars = maxChars - messageBox.value.length;
-        charCount.textContent = `${remainingChars} characters remaining`;
-    });
-});
 
