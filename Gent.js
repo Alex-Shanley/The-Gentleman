157bb59f6d4fcdff1376c6705c17d759cf1c690f
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-04-02 20:26:45 +0100

    mobile nav update

--- Gent.js
+++ Gent.js
@@ -253,3 +253,5 @@ function showSidebar(){
     sidebar.style.display = 'none'
   }
 
+
+ 
--- cart.js
+++ cart.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const messageBox = document.querySelector(".Message");
+    const charCount = document.createElement("div");
+    messageBox.parentNode.appendChild(charCount);
+
+    const maxChars = 300;
+
+    messageBox.addEventListener("input", function() {
+        const remainingChars = maxChars - messageBox.value.length;
+        charCount.textContent = `${remainingChars} characters remaining`;
+    });
+});
 
